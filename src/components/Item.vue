701551c26defd1a59a75352e2e2f1d3bfeb7989a
whoyoux <48690153+whoyoux@users.noreply.github.com>
2021-03-30 12:58:35 +0200

    Change Ad color

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="item rounded shadow card">
     <img src="https://www.cnet.com/a/img/-e95qclc6pwSnGE2YccC2oLDW_8=/1200x675/2020/04/16/7d6d8ed2-e10c-4f91-b2dd-74fae951c6d8/bazaart-edit-app.jpg" @click="click" class="card-img-top" alt="...">
-    <div class="row card-body" v-if="!isAd">
+    <div class="row card-body" v-if="!isAd" style="color: tomato">
 
         <div class="text-start col">
             <button class="btn btn-success" @click="addToCart">Add to cart</button>
